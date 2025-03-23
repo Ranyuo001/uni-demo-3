66cc29802cfc043116a6241d1db24be7c3c379ed
ranyuo001 <ranyuo002@163.com>
2025-03-23 17:48:32 +0800

    test01

--- src/pages/index/preview.vue
+++ src/pages/index/preview.vue
@@ -24,7 +24,7 @@
           <u-icon name="info" size="28"></u-icon>
           <view class="text">信息</view>
         </view>
-        <view class="box">
+        <view class="box" @click="showScore = true">
           <u-icon name="star-fill" size="28"></u-icon>
           <view class="text">分数</view>
         </view>
@@ -34,108 +34,150 @@
         </view>
       </view>
     </view>
-  </view>
-  <up-popup class="up-popup" :customStyle="popupCustomStyle" bgColor="#fff" mode="bottom" :show="show" @close="close"
-    @open="open">
-    <view class="infoPopup">
-      <view class="popHeader" style="display: flex;justify-content: space-between;">
-        <view></view>
-        <view class="title" style="color:#676767;font-size: 26rpx;">壁纸信息</view>
-        <view class="close">
-          <u-icon name="close" size="20" @click="close" style="padding: 6rpx;"></u-icon>
+    <up-popup class="up-popup" :customStyle="popupCustomStyle" bgColor="#fff" mode="bottom" :show="show" @close="close"
+      @open="open">
+      <view class="infoPopup">
+        <view class="popHeader" style="display: flex;justify-content: space-between;">
+          <view></view>
+          <view class="title" style="color:#676767;font-size: 26rpx;">壁纸信息</view>
+          <view class="close" @click="close">
+            <u-icon name="close" size="20" style="padding: 6rpx;"></u-icon>
+          </view>
         </view>
-      </view>
-      <scroll-view style="
+        <scroll-view style="
            max-height: 60vh;
 
         " scroll-y>
-        <view class="content">
-          <view class="row" style="display: flex;padding: 16rpx 0;
+          <view class="content">
+            <view class="row" style="display: flex;padding: 16rpx 0;
           font-size: 32rpx;line-height: 1.7em;">
-            <view class="label" style="
+              <view class="label" style="
               color: #a7a7a7;
               width: 140rpx;
               text-align: right;
               font-size: 30rpx;
-            ">壁纸ID:</view>
-            <text class="value" selectable style="flex: 1;width: 0;">1231341411</text>
-          
-          </view>
-          <view class="row" style="display: flex;padding: 16rpx 0;
+            ">壁纸ID：</view>
+              <text class="value" selectable style="flex: 1;width: 0;">1231341411</text>
+
+            </view>
+            <view class="row" style="display: flex;padding: 16rpx 0;
           font-size: 32rpx;line-height: 1.7em;">
-            <view class="label" style="
+              <view class="label" style="
               color: #a7a7a7;
               width: 140rpx;
               text-align: right;
               font-size: 30rpx;
             ">分类：</view>
-            <text class="value" selectable style="flex: 1;width: 0;">明星美女</text>
-          
-          </view>
-          <view class="row" style="display: flex;padding: 16rpx 0;
+              <text class="value" selectable style="flex: 1;width: 0;color: #28B389;">明星美女</text>
+
+            </view>
+            <view class="row" style="display: flex;padding: 16rpx 0;
           font-size: 32rpx;line-height: 1.7em;">
-            <view class="label" style="
+              <view class="label" style="
               color: #a7a7a7;
               width: 140rpx;
               text-align: right;
               font-size: 30rpx;
             ">发布者：</view>
-            <text class="value" selectable style="flex: 1;width: 0;">咸虾米</text>
-          
-          </view>
-          <view class="row" style="display: flex;padding: 16rpx 0;
+              <text class="value" selectable style="flex: 1;width: 0;">咸虾米</text>
+
+            </view>
+            <view class="row" style="display: flex;padding: 16rpx 0;
           font-size: 32rpx;line-height: 1.7em;">
-            <view class="label" style="
+              <view class="label" style="
               color: #a7a7a7;
               width: 140rpx;
               text-align: right;
               font-size: 30rpx;
             ">评分：</view>
-            <text class="value" selectable style="flex: 1;width: 0;">
-              <up-rate :count="Ratecount" v-model="Ratevalue"></up-rate>
-              <u-icon name="star-fill" size="" color="yellow"></u-icon>
-            </text>
-          
-          </view>
-          <view class="row" style="display: flex;padding: 16rpx 0;
+              <text class="value" selectable
+                style="flex: 1;width: 0;display: flex;align-items: center; gap: 20rpx; flex-wrap: nowrap;">
+                <up-rate readonly :count="Ratecount" v-model="Ratevalue" style="flex-shrink: 0;"></up-rate>
+              </text>
+
+            </view>
+            <view class="row" style="display: flex;padding: 16rpx 0;
           font-size: 32rpx;line-height: 1.7em;">
-            <view class="label" style="
+              <view class="label" style="
               color: #a7a7a7;
               width: 140rpx;
               text-align: right;
               font-size: 30rpx;
             ">摘要：</view>
-            <text class="value" selectable style="flex: 1;width: 0;">
-              摘要文字内容填充部分，摘要文字内容填充部分，摘要文字内容填充部分，摘要文字内容填充部分。
-            </text>
-          
-          </view>
-          <view class="row" style="display: flex;padding: 16rpx 0;
-          font-size: 32rpx;line-height: 1.7em;">
-            <text class="label"  style="color: #a7a7a7;width: 140rpx;text-align: right;font-size: 30rpx;">标签：</text>
-							<view class='value tabs'>
-								<view class="tab" v-for="item in 3">标签名</view>
-							</view>
-          </view>
-          <!-- <view class="copyright">声明：本图片来用户投稿，非商业使用，用于免费学习交流，如侵犯了您的权益，您可以拷贝壁纸ID举报至平台，邮箱513894357@qq.com，管理将删除侵权壁纸，维护您的权益。
-						
-          </view> -->
+              <text class="value" selectable style="flex: 1;width: 0;">
+                摘要文字内容填充部分，摘要文字内容填充部分，摘要文字内容填充部分，摘要文字内容填充部分。
+              </text>
 
+            </view>
+            <view class="row" style="display: flex;padding: 16rpx 0;
+          font-size: 32rpx;line-height: 1.7em;">
+              <text class="label" style="color: #a7a7a7;width: 140rpx;text-align: right;font-size: 30rpx;">标签：</text>
+              <view class='value c' style="display: flex;flex-wrap: nowrap;">
+                <view class="tab" v-for="item in 3" style="color: #28B389;border: 1px solid #28B389 ;
+              font-size: 22rpx;
+              padding: 10px 30rpx;
+              border-radius: 40rpx;
+              line-height: 1em;
+              margin: 0 10rpx 10rpx 0;">标签名</view>
+              </view>
+            </view>
+            <view class="copyright" style="
+            font-size: 28rpx;
+            padding: 20rpx;
+            background: #f6f6f6;
+            color: #666;
+            border-bottom: 10rpx;
+            margin: 20rpx 0;
+            line-height: 1.6em;
+          ">
+              声明：本图片来用户投稿，非商业使用，用于免费学习交流，如侵犯了您的权益，您可以拷贝壁纸ID举报至平台，邮箱513894357@qq.com，管理将删除侵权壁纸，维护您的权益。
+            </view>
+
+          </view>
+        </scroll-view>
+      </view>
+    </up-popup>
+
+    <up-popup @click="clickScore" @close="clickCloseScore" :show="showScore" class="scorePopup">
+      <view class="scorePopup">
+        <view class="popHeader" style="display: flex;justify-content: space-between;">
+          <view></view>
+          <view class="title" style="color:#676767;font-size: 26rpx;">壁纸评分</view>
+          <view class="close" @click="clickCloseScore">
+            <u-icon name="close" size="20" style="padding: 6rpx;"></u-icon>
+          </view>
         </view>
-      </scroll-view>
-    </view>
-  </up-popup>
+      </view>
+
+      <view class="content">
+        <up-rate :count="userScore" v-model="scoreRatevalue"></up-rate>
+
+      </view>
+
+      <view class="footer">
+
+      </view>
+
+    </up-popup>
+  </view>
 </template>
 
 <script lang='ts' setup>
 import { ref } from 'vue'
 const date = ref(String(Date.now()))
 const maskState = ref(true);
-const show = ref(true);
+
 
 //评分选择
-const Ratecount = ref(4);  //参数设置总共有多少颗星星可选择
-const Ratevalue = ref(2);  //双向绑定初始化时默认选中的星星数量
+const Ratecount = ref(5);  //参数设置总共有多少颗星星可选择
+const Ratevalue = ref(3.5);  //双向绑定初始化时默认选中的星星数量
+
+//评分弹窗
+const userScore = ref(5);  //参数设置总共有多少颗星星可选择
+const scoreRatevalue = ref(3.5);  //双向绑定初始化时默认选中的星星数量
+
+// 定义 show 变量
+const show = ref(false);
 
 // 定义方法  
 function open() {
@@ -150,10 +192,26 @@ function close() {
   // console.log('close');  
 }
 
+
+
+const showScore = ref(true)
+
+//评分弹窗
+const clickScore = () => {
+  showScore.value = true
+}
+
+//关闭评分弹窗
+const clickCloseScore = () => {
+  showScore.value = false
+}
+
 //点击info弹窗
 // const clickInfo = () =>{
 //   infoPopup.value.open
 // }
+
+
 
 //遮罩
 const maskChange = () => {
@@ -204,6 +262,7 @@ const popupCustomStyle = ref({
 
     .goBack {
       // 这里可以添加返回按钮的样式
+
     }
 
     .count {
@@ -269,7 +328,6 @@ const popupCustomStyle = ref({
 
         .close {
           padding: 10rpx;
-          border: 1px solid black;
         }
       }
 
